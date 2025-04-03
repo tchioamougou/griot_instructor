0a68991ee37ab7178c8bfa2fdd500915746f7630
Mel-aine <melaineevans7@gmail.com>
2025-04-03 17:11:39 +0100

    commit

---
+++ src/components/layout/header/HeaderLogo.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-link to="/" class="lg:hidden flex items-center gap-2">
+  <!-- Logo en mode clair -->
+  <img class="dark:hidden rounded-full w-10" src="/src/assets/images/header/logo2.png" alt="Logo" />
+
+
+
+  <!-- Logo en mode sombre -->
+  <img class="hidden dark:block rounded-full w-10" src="/src/assets/images/header/logo2.png" alt="Logo" />
+    <!-- Texte -->
+    <span class="inline-flex text-xl text-purple-500 font-bold">Enjoy</span>
+</router-link>
+
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
