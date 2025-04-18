165df5df4b3c2085fd507e775ff0c6dcdaa3c846
Mel-aine <melaineevans7@gmail.com>
2025-04-18 17:20:33 +0100

    comment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,29 @@
-import { fileURLToPath, URL } from 'node:url'
+// import { fileURLToPath, URL } from 'node:url'
 
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+// import vueJsx from '@vitejs/plugin-vue-jsx'
+// import vueDevTools from 'vite-plugin-vue-devtools'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//     vueJsx(),
+//     vueDevTools(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     },
+//   },
+// })
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -16,5 +34,13 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT || '5173'),
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT || '4173'),
   },
 })
